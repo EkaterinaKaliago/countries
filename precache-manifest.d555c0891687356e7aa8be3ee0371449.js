2bbd1186becaad64d629a5552b1a9c70b9bf0c1b
EkaterinaKaliago <kaliago@yandex.ru>
2020-04-02 15:13:14 +0300

    Updates

---
+++ precache-manifest.d555c0891687356e7aa8be3ee0371449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e57c9d5f435c9d18e28469ea49ebd53",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "369a196c81df90c0ef13",
+    "url": "/countries/static/css/main.8ed521ce.chunk.css"
+  },
+  {
+    "revision": "9b7dca1409634d5b6ba1",
+    "url": "/countries/static/js/2.9e7b7897.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/countries/static/js/2.9e7b7897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369a196c81df90c0ef13",
+    "url": "/countries/static/js/main.06942799.chunk.js"
+  },
+  {
+    "revision": "4b3618ad484b042b9caa",
+    "url": "/countries/static/js/runtime-main.fa1d700f.js"
+  },
+  {
+    "revision": "2ae46ecc9786ada70bdf9a269cc2ceb9",
+    "url": "/countries/static/media/arrow_back.2ae46ecc.svg"
+  }
+]);
