a7abbfe558be8fee58004df77a325db01fd9262b
EkaterinaKaliago <kaliago@yandex.ru>
2020-04-11 17:20:56 +0300

    add pagination

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -8,6 +8,7 @@ import styles from "./HomePage.module.css";
 type State = {
   countries: Country[],
   searchText: string,
+  currentPage: number,
 };
 
 class HomePage extends Component<{}, State> {
@@ -15,6 +16,7 @@ class HomePage extends Component<{}, State> {
   state: State = {
     countries: [],
     searchText: '',
+    currentPage: 1,
   };
 
   onChanged = async (e: any) => {
@@ -34,13 +36,83 @@ class HomePage extends Component<{}, State> {
     }
   };
 
+  onPrev = () => {
+    this.setState({currentPage: this.state.currentPage - 1});
+  };
+
+  onNext = () => {
+    this.setState({currentPage: this.state.currentPage + 1});
+  };
+
+  changePage = (number: any) => {
+    this.setState({currentPage: number});
+  };
+
   render() {
+
+    const countriesPerPage = 6;
+    const startIndex = countriesPerPage * this.state.currentPage - 1;
+    const endIndex = startIndex + countriesPerPage;
+    const currentCounties = this.state.countries.slice(startIndex + 1, endIndex + 1);
+    const totalNumPages = this.state.countries.length / 6;
+    const navLinks = [];
+
+    for (let i = 1; i <= totalNumPages; i++) {
+
+      if (i > 1 && i < this.state.currentPage - 2) {
+        if (i === 2) {
+          navLinks.push(
+            <span key={i}>
+              ...
+            </span>
+          );
+        }
+        continue;
+      }
+
+      if (i < totalNumPages - 1 && i > this.state.currentPage + 2) {
+        if (totalNumPages - 2 < i) {
+          navLinks.push(
+            <span key={i}>
+              ...
+            </span>
+          );
+        }
+        continue;
+      }
+
+      navLinks.push(
+        <button key={i}
+          type="button"
+          onClick={() => this.changePage(i)}
+          className={`${styles.paginationNum} ${i === this.state.currentPage ? styles.active : ''}`}>
+          {i}
+        </button>
+      );
+    }
+
     return (
       <main className={styles.main}>
         <h1 className={styles.title}>Search for a country</h1>
         <SearchInput value={this.state.searchText} onChanged={this.onChanged} />
         <div className={styles.countries}>
-          <CountryList countries={this.state.countries} />
+          {this.state.countries.length > 6 ? <div className={styles.pagination}>
+            <button type="button"
+              className={styles.btn}
+              disabled={this.state.currentPage - 1 <= 0} onClick={this.onPrev}>
+              prev
+            </button>
+            <div className={styles.paginationNumWrap}>
+              {navLinks}
+            </div>
+            <button type="button"
+              className={styles.btn}
+              disabled={this.state.currentPage + 1 >= totalNumPages}
+              onClick={this.onNext}>
+              next
+            </button>
+          </div> : ''}
+          <CountryList countries={currentCounties} />
         </div>
       </main>
     );
